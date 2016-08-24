09c840259055e5d42b9a36a09259dc683420dfa4
madachuan <madachuanfanqiang@gmail.com>
2016-08-24 16:34:20 +0800

    This is a coordinate transformation from WGS-84 to ENU.
    The ENU(East-North-Up) means local Cartesian coordinates coordinate system.

---
+++ coor.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <math.h>
+
+#include "coor.h"
+
+struct coor blh2xyz(struct coor blh)
+{
+	double a = A_WGS84;
+	double f = F_WGS84;
+	double b = a * (1 - f);
+	double e2 = 1 - b * b / a / a;
+	double B = blh.x;
+	double L = blh.y;
+	double H = blh.z;
+	double N = a / sqrt(1 - e2 * sin(B) * sin(B));
+	struct coor xyz;
+	xyz.x = (N + H) * cos(B) * cos(L);
+	xyz.y = (N + H) * cos(B) * sin(L);
+	xyz.z = (N * (1 - e2) + H) * sin(B);
+	return (xyz);
+}
+
+struct coor blh2enu(struct coor blh1, struct coor blh2)
+{
+	struct coor xyz1 = blh2xyz(blh1);
+	struct coor xyz2 = blh2xyz(blh2);
+	double dx = xyz2.x - xyz1.x;
+	double dy = xyz2.y - xyz1.y;
+	double dz = xyz2.z - xyz1.z;
+	double B = blh1.x;
+	double L = blh1.y;
+	struct coor enu;
+	enu.x = -sin(L) * dx + cos(L) * dy;
+	enu.y = -sin(B) * cos(L) * dx - sin(B) * sin(L) * dy + cos(B) * dz;
+	enu.z = cos(B) * cos(L) * dx + cos(B) * sin(L) * dy + sin(B) * dz;
+	return (enu);
+}
---
+++ coor.h
@@ -0,0 +1,8 @@
+typedef struct coor {
+	double x;
+	double y;
+	double z;
+};
+
+#define A_WGS84 = 6378137.0;
+#define F_WGS84 = 1.0 / 298.257223565;
