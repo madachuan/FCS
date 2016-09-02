934e2dee7d60371986dbf6a8a3c7818d198ef018
madachuan <madachuanfanqiang@gmail.com>
2016-09-02 10:44:59 +0800

    Add an ISR of the timer and an ISR of CAN receiving.

---
+++ isr.c
@@ -0,0 +1,29 @@
+#include <vxWorks.h>
+#include <semLib.h>
+#include <msgQLib.h>
+
+unsigned char tick;
+unsigned short counter;
+
+void isr_tmr(void)
+{
+	tick++;
+	counter++;
+	if (counter > 749)
+		counter = 0;
+	semFlush(bs_tmr);
+}
+
+void isr_can(void)
+{
+	static char buf[13];
+	if (!(INTERRUPT() & 0x01))
+		return;
+	if (!HK_RD_IR(1)) {
+		RESET();
+		can();
+		return;
+	}
+	HK_CAN_RECEIVE(1, buf);
+	msgQSend(mq_can, buf, 13, NO_WAIT, MSG_PRI_NORMAL);
+}
