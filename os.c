d8e9c88296768201bf286f608d28014b51badd66
madachuan <madachuanfanqiang@gmail.com>
2016-09-02 09:48:00 +0800

    Add a message queue and a task for the ISR of CAN receiving.
    What I want to do is to reduce time consuming of the ISR. And it's not a good idea to use malloc in an ISR.
    
    After adding a task, modify priorities of tasks.
    The task_can's  priority is 100,
    The task_gps's  priority is 110,
    The task_accs's priority is 120,
    The task eots's priority is 130,
    The task mls's  priority is 140,
    The taks_sync's priority is 150.

--- os.c
+++ os.c
@@ -1,9 +1,13 @@
 #include <vxWorks.h>
 #include <semLib.h>
+#include <msgQLib.h>
 #include <taskLib.h>
 
 SEM_ID bs_tmr;
 
+MSG_Q_ID mq_can;
+
+int_tid_can;
 int tid_gps;
 int tid_accs;
 int tid_eots;
@@ -13,14 +17,17 @@ int tid_sync;
 void os(void)
 {
 	bs_tmr = semBCreate(SEM_EMPTY, SEM_Q_PRIORITY);
-	tid_gps = taskSpawn("gps", 100, VX_FP_TASK, 10000, (FUNCPTR)task_gps,
+	mq_can = msgQCreate(1, 13, MSG_Q_FIFO);
+	tid_can = taskSpawn("can", 100, VX_FP_TASK, 10000, (FUNCPTR)task_can,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	tid_accs = taskSpawn("accs", 110, VX_FP_TASK, 10000, (FUNCPTR)task_accs,
+	tid_gps = taskSpawn("gps", 110, VX_FP_TASK, 10000, (FUNCPTR)task_gps,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	tid_eots = taskSpawn("eots", 120, VX_FP_TASK, 10000, (FUNCPTR)task_eots,
+	tid_accs = taskSpawn("accs", 120, VX_FP_TASK, 10000, (FUNCPTR)task_accs,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	tid_mls = taskSpawn("mls", 130, VX_FP_TASK, 10000, (FUNCPTR)task_mls,
+	tid_eots = taskSpawn("eots", 130, VX_FP_TASK, 10000, (FUNCPTR)task_eots,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	tid_sync = taskSpawn("sync", 140, VX_FP_TASK, 10000, (FUNCPTR)task_sync,
+	tid_mls = taskSpawn("mls", 140, VX_FP_TASK, 10000, (FUNCPTR)task_mls,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	tid_sync = taskSpawn("sync", 150, VX_FP_TASK, 10000, (FUNCPTR)task_sync,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 }
