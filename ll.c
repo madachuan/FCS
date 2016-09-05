04b0bcbb8628c91508461c4e46c98da52f1ff690
madachuan <madachuanfanqiang@gmail.com>
2016-09-05 09:59:21 +0800

    Add operations about the dual (circular) linked list through precompile.
    Why I choose precompile is for saving the memory on the situation without the dual (circular) linked list.

--- ll.c
+++ ll.c
@@ -7,6 +7,9 @@ struct ll *ll_born(void)
 	struct ll *head = malloc(sizeof(struct ll));
 	head->key = NULL;
 	head->next = NULL;
+#ifdef DUAL_LINKED_LIST
+	head->prev = NULL;
+#endif
 	return (head);
 }
 
@@ -15,6 +18,9 @@ struct ll *cll_born(void)
 	struct ll *head = malloc(sizeof(struct ll));
 	head->key = NULL;
 	head->next = head;
+#ifdef DUAL_LINKED_LIST
+	head->prev = head;
+#endif
 	return (head);
 }
 
@@ -25,6 +31,10 @@ struct ll *ll_ins(struct ll *head, struct ll *(*born)())
 		head->next = malloc(sizeof(struct ll));
 		head->next->key = NULL;
 		head->next->next = tmp;
+#ifdef DUAL_LINKED_LIST
+		head->next->prev = head;
+		tmp->prev = head->next;
+#endif
 		return (head);
 	}
 	return (born());
@@ -43,6 +53,9 @@ struct ll *ll_del(struct ll *head)
 		if (head->next && head->next != head) {
 			struct ll *tmp = head->next;
 			head->next = tmp->next;
+#ifdef DUAL_LINKED_LIST
+			tmp->next->prev = head;
+#endif
 			free(tmp);
 			return (head);
 		}
--- ll.h
+++ ll.h
@@ -1,4 +1,9 @@
+#define DUAL_LINKED_LIST
+
 struct ll {
 	void *key;
 	struct ll *next;
+#ifdef DUAL_LINKED_LIST
+	struct ll *prev;
+#endif
 };
