7585513024d2935f3fcb59e4518f4c7165d19247
madachuan <madachuanfanqiang@gmail.com>
2016-09-19 16:26:37 +0800

    Remove the temporary buffer called "pre".

--- task_gps.c
+++ task_gps.c
@@ -23,46 +23,44 @@ double rd_gps(char *data, unsigned n)
 
 void task_gps(void)
 {
-	static char pre[256];
 	static char buf[256];
 	for (;;) {
 		static unsigned sum;
-		unsigned n = read(com4, pre + sum, 256 - sum);
-		if (sum == 0 && strstr(pre, "$GPHPD,") != pre)
+		unsigned n = read(com4, buf + sum, 256 - sum);
+		if (sum == 0 && strstr(buf, "$GPHPD,") != buf)
 			continue;
 		sum += n;
 		if (sum > 256)
 			goto CLEAN;
-		char *end = strstr(pre, "\r\n");
+		char *end = strstr(buf, "\r\n");
 		if (!end)
 			continue;
 		if (*(end - 3) != '*')
 			goto CLEAN;
 		if (*(end - 2) >= '0' && *(end - 2) <= '9')
-			if (*(end - 2) - 48 != chkxor(pre + 1,
-						end - 4 - pre) >> 4) {
+			if (*(end - 2) - 48 != chkxor(buf + 1,
+						end - 4 - buf) >> 4) {
 				goto CLEAN;
 		else if (*(end - 2) >= 'A' && *(end - 2) <= 'F')
-			if (*(end - 2) - 55 != chkxor(pre + 1,
-						end - 4 - pre) >> 4)
+			if (*(end - 2) - 55 != chkxor(buf + 1,
+						end - 4 - buf) >> 4)
 				goto CLEAN;
 		else
 			goto CLEAN;
 		if (*(end - 1) >= '0' && *(end - 1) <= '9')
-			if (*(end - 1) - 48 != (chkxor(pre + 1,
-						end - 4 - pre) & 0x0F))
+			if (*(end - 1) - 48 != (chkxor(buf + 1,
+						end - 4 - buf) & 0x0F))
 				goto CLEAN;
 		else if (*(end - 1) >= 'A' && *(end - 1) <= 'F')
-			if (*(end - 1) - 55 != (chkxor(pre + 1,
-						end - 4 - pre) & 0x0F))
+			if (*(end - 1) - 55 != (chkxor(buf + 1,
+						end - 4 - buf) & 0x0F))
 				goto CLEAN;
 		else
 			goto CLEAN;
 		counter = (rd_gps(2) - (int)rd_gps(2)) * 1000;
-		memcpy(buf, pre, 256);
 		current = wr_sm(current, "gps_rx", buf, 256);
 CLEAN:
 		sum = 0;
-		memset(pre, 0x00, 256);
+		memset(buf, 0x00, 256);
 	}
 }
